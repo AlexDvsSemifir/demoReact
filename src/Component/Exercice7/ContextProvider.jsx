a3e7b6f23801908599fbd1163b8ea2981fea2643
Oh_My_Goat <daftouney@gmail.com>
2022-01-31 11:31:22 +0100

    fix(Exercice7): Remise en forme de l'exercice et ajout de paragraphes explicatifs

--- src/Component/Exercice7/ContextProvider.jsx
+++ src/Component/Exercice7/ContextProvider.jsx
@@ -20,6 +20,9 @@ export const ContextProvider = () => {
     return ( 
         <Fragment>
             <h2>Exercice 7</h2>
+            <hr />
+            <h3>ContextProvider</h3>
+            <p>Context Provider qui stocke les thèmes, et va les mettre à disposition des héritiers demandeurs</p>
             <themeContext.Provider value={themes}>
                 <ContextIntermediaire />
             </themeContext.Provider>
